2c12ead515bc54fa474aabed10de95208c7c26d1
Anna Hangstefer <anna-hangstefer@lambdastudents.com>
2020-06-06 20:08:43 -0400

    3 events complete

--- js/index.js
+++ js/index.js
@@ -1,1 +1,27 @@
 // Your code goes here
+const bigH4 = document.querySelectorAll('h4');
+console.log(bigH4);
+
+bigH4.forEach((h) =>{
+    h.addEventListener('mouseover', (e) => {
+        h.style.transform = "scale(1.5)";
+        h.style.transition = "transform 2s";
+        e.stopPropagation();
+  });
+});
+
+bigH4.forEach((h)=>{
+    h.addEventListener('mouseleave',(e) => {
+        h.style.transform = "initial";
+        e.stopPropagation();
+    });
+});
+
+const logo = document.querySelector('.logo-heading');
+console.log(logo);
+
+document.addEventListener('keypress',(e)=> {
+    // if (e.key==='f'){
+     logo.style.color = 'gold';
+    // }
+});
